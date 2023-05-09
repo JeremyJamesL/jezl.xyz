2d79aa9d4cd3f2e2d9f6f54c1a3555205a2977f2
JeremyJamesL <jeremyluscombe@gmail.com>
2023-05-09 21:33:12 +0100

    added SE pdf and fixed theme toggler

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const modeToggle = document.querySelector('.form-check-input');
 const modeToggleLabel = document.querySelector('.form-check-label');
 const htmlDoc = document.querySelector('html');
 
-modeToggle.addEventListener('change', (e) => {
-    if(e.target.checked) {
+modeToggle.addEventListener('change', () => {
+    const activeTheme = htmlDoc.getAttribute('data-bs-theme');
+    if(activeTheme === 'light') {
         localStorage.setItem("theme", "dark");
         htmlDoc.setAttribute("data-bs-theme", "dark")
         modeToggleLabel.innerHTML = '<i class="fa-regular fa-sun fs-4 text-white"></i>'
@@ -12,13 +13,10 @@ modeToggle.addEventListener('change', (e) => {
         htmlDoc.setAttribute("data-bs-theme", "light")
         modeToggleLabel.innerHTML = '<i class="fa-regular fa-moon fs-4 text-white"></i>'
     }
-    // console.log(localStorage.getItem("theme"))
 })
 
 window.addEventListener("DOMContentLoaded", () => {
     const theme = localStorage.getItem("theme");
-    htmlDoc.setAttribute("data-bs-theme", theme);
-    console.log(theme);
     if(theme === "dark") {
         modeToggleLabel.innerHTML = '<i class="fa-regular fa-sun fs-4 text-white"></i>'
         return
